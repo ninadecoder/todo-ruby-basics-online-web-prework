eced69cbac31e416df19c5b1ee55796592cab595
Nina Kazzim <kazzim@icloud.com>
2019-05-10 01:03:53 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  num = num1/num2
+  return num
 end
 
 def assign_variable(value)
-  
+  variable = value
+  return variable
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
-
+def greeting(stfu1, stfu2)
+   
 end
 
-def return_a_value
-
+def return_a_value()
+  return "Nice"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value()
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party("cheese")
+  return 
 end
